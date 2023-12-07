b91cdb2dc7aee675f848bcaf67ee483ac8b60eb8
AlanEA04 <omar.encinas.cetis69@gmail.com>
2023-12-07 10:03:01 -0700

    Agregar archivos de proyecto.

---
+++ ArregloPersona.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ArregloPersona
+    {
+
+        private int[] miMatriz;
+        int valores;
+        public ArregloPersona(int tamaño)
+        {
+           miMatriz = new int [tamaño];
+           //tamaño = posocion
+        }
+
+        public void Insertar(int posicion)
+        {
+            int i;
+
+            for (i = 0; posicion < miMatriz.Length;)
+            {   
+                miMatriz[i] = posicion;
+                
+                miMatriz[posicion] = valores;   
+                Console.WriteLine($"ingresa los valores de la posicion {posicion} ");
+               
+
+            }
+            
+                //if (valores != null)
+                //{
+
+
+                //    Console.WriteLine($"ingresa los valor de la posicion{posicion}");
+                //    valores = miMatriz[valores];
+                //    miMatriz[valores] = posicion;
+                //    posicion++;                                          
+                //}
+         
+        }
+
+        public void Mostrar() 
+        {
+            miMatriz = miMatriz;
+            valores = miMatriz[valores];
+           
+        
+        }
+
+
+        public  void ValorBuscado(int Buscado)
+        {
+
+
+
+
+        }
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa el tamaño de la matriz");
+             int tamaño = int .Parse(Console.ReadLine());
+
+          //  ArregloPersona miMatriz = new ArregloPersona();
+
+            Console.WriteLine("ingresa los  datos  a la matriz");
+            int valores = int .Parse(Console.ReadLine());
+
+         //   miMatriz.Mostrar();
+
+            Console.WriteLine("Ingresa el elemento a buscar");
+            int buscado = int .Parse(Console.ReadLine());
+
+
+
+        }
+    }
+}
